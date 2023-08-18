85ddb676349bf05fb66292a9cb170a02fe13cb38
kimng1003 <kimnagyeong50@gmail.com>
2023-08-18 18:06:54 +0900

    캐시 삭제

--- highcare/src/main/java/com/highright/highcare/auth/repository/RefreshTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.highright.highcare.auth.repository;
-
-import com.highright.highcare.auth.entity.RefreshToken;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
-
-
-}
--- highcare/src/main/java/com/highright/highcare/auth/service/AuthService.java
+++
@@ -1,8 +0,0 @@
-package com.highright.highcare.auth.service;
-
-import com.highright.highcare.auth.dto.LoginMemberDTO;
-
-public interface AuthService {
-
-    Object login(LoginMemberDTO loginMemberDTO);
-}
