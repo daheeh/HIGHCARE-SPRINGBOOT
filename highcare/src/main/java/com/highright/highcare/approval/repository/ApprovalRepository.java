669080b5f6258bcf4fc565a9e8cabbf732724083
kimng1003 <kimnagyeong50@gmail.com>
2023-08-18 18:05:04 +0900

    캐시삭제

--- highcare/src/main/java/com/highright/highcare/approval/repository/ApprovalRepository.java
+++
@@ -1,8 +0,0 @@
-package com.highright.highcare.approval.repository;
-
-import com.highright.highcare.approval.entity.ApvForm;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ApprovalRepository extends JpaRepository<ApvForm, Integer> {
-
-}
--- highcare/src/main/java/com/highright/highcare/jwt/TokenProvider.java
+++
@@ -1,7 +0,0 @@
-package com.highright.highcare.jwt;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class TokenProvider {
-}
