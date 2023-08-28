ce40c3f5b9ed5df930a55df7285668a030f51dc8
{daheeh} <odaheei@gmail.com>
2023-08-28 15:10:54 +0900

    application.yml 오류수정

--- highcare/src/main/java/com/highright/highcare/config/RedisConfig.java
+++ highcare/src/main/java/com/highright/highcare/config/RedisConfig.java
@@ -8,8 +8,7 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 @EntityScan(basePackages = "com.highright.highcare")
 @EnableRedisRepositories(basePackages = "com.highright.highcare")
 public class RedisConfig {
-<<<<<<< HEAD
+
 }
-=======
-}
->>>>>>> 4c170c2b8ba36b7a04f3dc1a13c2fd927f9c8d47
+
+
