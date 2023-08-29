2d61860dbe5d72555038b1a8fdefe683edd88a81
DESKTOP-EM2UUI3\chohyeran <gpfks765@gmail.com>
2023-08-29 15:50:04 +0900

    profile DB

--- highcare/src/main/java/com/highright/highcare/mypage/Repository/ProfileRepository.java
+++ highcare/src/main/java/com/highright/highcare/mypage/Repository/ProfileRepository.java
@@ -4,10 +4,12 @@ import com.highright.highcare.mypage.dto.MyProfileDTO;
 import com.highright.highcare.mypage.entity.MyProfile;
 import com.highright.highcare.mypage.entity.MyProfileFile;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface ProfileRepository extends JpaRepository<MyProfile, Integer> {
 
     MyProfile findByEmpNo(int empNo);
--- highcare/src/main/java/com/highright/highcare/mypage/entity/MyProfileFile.java
+++ highcare/src/main/java/com/highright/highcare/mypage/entity/MyProfileFile.java
@@ -1,8 +1,6 @@
 package com.highright.highcare.mypage.entity;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 import org.springframework.context.annotation.Profile;
 
 import javax.persistence.*;
@@ -14,33 +12,32 @@ import java.sql.Date;
 @Getter
 @Setter
 @SequenceGenerator(
-        name = "FILE_SEQ_NO",       // 엔티티 안에서 저장되는 이름
+        name = "MYFILE_SEQ_NO",       // 엔티티 안에서 저장되는 이름
         sequenceName = "FILE_SEQ_NO",       // DB에 저장되는 이름
         initialValue = 1, allocationSize = 1
 )
+@NoArgsConstructor
+@AllArgsConstructor
 @ToString
 public class MyProfileFile {
 
     @Id
     @Column (name = "PHOTO_FILE_CODE")
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "FILE_SEQ_NO")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MYFILE_SEQ_NO")
     private int photoCode;
 
     @Column(name="PROFILE_CODE")
     private int code;
 
     @Column(name = "ORIGINAL_FILE_NAME")
-    private String orName;
+    private String name;
 
     @Column(name = "CHANGED_FILE_NAME")
     private String chName;
 
-    @Column(name = "CREATION_DATE")
+    @Column(name = "CREATION_DATE" ,nullable = true)
     private Date date;
 
-//    @OneToOne
-//    @JoinColumn(name = "PROFILE_CODE")
-//    private MyProfile myProfile;
 
 
 }
