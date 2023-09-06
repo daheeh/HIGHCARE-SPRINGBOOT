5bd1f41e8809c72c8b3f4afb4f4a6ac6a323e429
{daheeh} <odaheei@gmail.com>
2023-09-06 19:36:25 +0900

    #10 WebSocketConfig 오류 수정

--- highcare/src/main/java/com/highright/highcare/config/WebConfiguration.java
+++ highcare/src/main/java/com/highright/highcare/config/WebConfiguration.java
@@ -1,24 +1,32 @@
 package com.highright.highcare.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebConfiguration implements WebSocketMessageBrokerConfigurer {
+@RequiredArgsConstructor
+public class WebConfiguration implements WebMvcConfigurer {
+
+    private final String[] CLASSPATH_RESOURCE_LOCATIONS = { "classpath:/META-INF/resources/",
+            "classpath:/resources/", "classpath:/static/", "classpath:/public/" };
+
+    @Value("${image.add-resource-locations}")
+    private String ADD_RESOURCE_LOCATION;
+
+    @Value("${image.add-resource-handler}")
+    private String ADD_RESOURCE_HANDLER;
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat").setAllowedOriginPatterns("*").withSockJS();
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        // 이미지 경로 매핑
+        registry.addResourceHandler(ADD_RESOURCE_HANDLER)
+                .addResourceLocations(ADD_RESOURCE_LOCATION);
     }
 
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/topic","/queue");
-    }
+
 
 }
